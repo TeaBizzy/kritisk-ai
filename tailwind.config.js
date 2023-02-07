5754e39362c9a4e578098abf41d3a12790e7ef51
Stefan Talbot <satalbot@protonmail.com>
2023-02-07 15:43:41 -0500

    Implement Navbar Component
    Feat: Add font to tailwind.
    Feat: Add navbar component to app.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
   theme: {
     extend: {},
+    fontFamily: {
+      logo: ['Anton', 'sans-serif']
+    }
   },
   plugins: [],
 }
