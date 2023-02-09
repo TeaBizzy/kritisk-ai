d88cbf3a641c5c49b7a66a52db97e0f2b7bc532c
Stefan Talbot <satalbot@protonmail.com>
2023-02-09 13:29:51 -0500

    Update Site Style

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChange } from '@angular/core';
+import { predict } from 'src/utils/img-classifier';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-body',
@@ -7,8 +9,57 @@ import { Component } from '@angular/core';
 })
 export class BodyComponent {
   fileName: string = '';
+  imgSrc: any = '';
+  prediciton: any;
+  chartData: any;
+
+  ngOnInit() {
+    this.createChart()
+  }
+
+  createChart() {
+    // Renders right to the canvas element with id='chart'
+    this.chartData = new Chart('chart',
+      {
+        type: "doughnut",
+        data: {
+          labels: ['Dated', 'Modern', 'Very Dated'],
+          datasets: [{
+            data: [],
+            backgroundColor: [
+              'rgb(168, 85, 247)',
+              'rgb(139, 92, 246)',
+              'rgb(217, 70, 239)'
+            ]
+          }],
+        },
+        options: {
+          plugins: {
+            legend: {
+              display: false
+            }}}
+      })
+  }
 
   onUpload(event: any): void {
-    this.fileName = event.target.files[0].name;
+    this.fileName = event.target.files[0].name
+    const reader = new FileReader();
+
+    reader.addEventListener("load", () => {
+      this.imgSrc = reader.result;
+
+      let img = new Image();
+      img.src = typeof(reader.result) === 'string' ? reader.result : '';
+
+      predict(img)
+        .then((prediction: any) => {
+          this.prediciton = prediction;
+          this.chartData.data.datasets[0].data = this.prediciton.map((result: any) => result.probability)
+          this.chartData.update()
+        })
+        .catch((err: any) => {})
+    })
+
+    reader.readAsDataURL(event.target.files[0])
   }
 }
--- src/utils/img-classifier.ts
+++ src/utils/img-classifier.ts
@@ -1,26 +1,22 @@
+import * as tf from '@tensorflow/tfjs';
 import * as tm from '@teachablemachine/image';
+import { mod } from '@tensorflow/tfjs';
 
-let model: any = null
+const URL = 'https://teachablemachine.withgoogle.com/models/TjwAEeUqe/'
+const modelURL = URL + 'model.json'
+const metadataURL = URL + 'metadata.json'
 
-export async function initModel() {
-  const URL = 'https://teachablemachine.withgoogle.com/models/TjwAEeUqe/'
-  const modelURL = URL + 'model.json'
-  const metadataURL = URL + 'metadata.json'
 
-  model = await tm.load(modelURL, metadataURL);
-  console.log(model);
-}
-
-initModel()
-
-export function predict(img:any) {
+export async function predict(img:any) {
+  const model = await tm.load(modelURL, metadataURL);
   return model.predict(img, false)
     .then((prediction:any) => {
+      console.log('wtf');
       for (const value of prediction) {
         value.probability = Math.round(value.probability * 100)
       }
       return prediction
-    })
+    }).catch((err:any) => {console.log(err)})
 }
 
 
