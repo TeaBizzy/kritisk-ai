4cf9bf83d68c027dccb735ef4602c71674a27bbf
Stefan Talbot <satalbot@protonmail.com>
2023-04-19 11:07:17 -0400

    Add Developer Contact

--- src/app/dev-list/dev-list.component.ts
+++ src/app/dev-list/dev-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dev-list',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./dev-list.component.css']
 })
 export class DevListComponent {
-
+  @Input() foo: any;
 }
