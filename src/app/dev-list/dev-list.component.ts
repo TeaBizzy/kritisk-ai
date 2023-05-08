24533ccf3dab704157e650b08e0644b197297694
Stefan Talbot <satalbot@protonmail.com>
2023-05-08 09:43:21 -0400

    Abstract Dev Contacts, and Dynamically Render Contacts

--- src/app/dev-list/dev-list.component.ts
+++ src/app/dev-list/dev-list.component.ts
@@ -1,8 +1,11 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component } from '@angular/core';
+import { developers } from 'src/data/developers';
 
 @Component({
   selector: 'app-dev-list',
   templateUrl: './dev-list.component.html',
 })
+
 export class DevListComponent {
+  developers = developers;
 }
